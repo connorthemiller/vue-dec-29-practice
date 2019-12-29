80a5cd726335ffc70c78d1321c54343682045535
connorthemiller <connormiller@Connors-Mac-mini.local>
2019-12-29 10:31:22 -0800

    added the server

---
+++ server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "plugin:vue/essential",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
