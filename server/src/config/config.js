013995aadc32578dc66ceee337e5b4efd516e464
connorthemiller <checkthegate21@gmail.com>
2019-12-30 10:32:02 -0800

    setting up database continued

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  port: 8081,
+  port: process.env.PORT || 8081,
   db: {
-    database: process.env.DB_NAME || 'mycoolapp'
-    user:
-    password:
+    database: process.env.DB_NAME || 'mycoolapp',
+    user: process.env.DB_USER || 'mycoolapp',
+    password: process.env.DB_PASS || 'mycoolapp',
     options: {
-      
+      dialect: process.env.DIALECT || 'sqlite',
+      host: process.env.HOST || 'localhost',
+      storage: './mycoolapp.sqlite'
     }
   }
 }
