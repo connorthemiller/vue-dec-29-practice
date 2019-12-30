b25d29895d66ba2273f08a6d1694967739231a82
connorthemiller <checkthegate21@gmail.com>
2019-12-30 10:50:17 -0800

    added controllers

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,7 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+
 module.exports = (app) => {
-  app.post('/register', (req, res) => {
-    res.send({
-        message: `hello ${req.body.email}! have fun.`
-    })
-  })
+  app.post('/register',
+    AuthenticationController.register
+  )
 }
