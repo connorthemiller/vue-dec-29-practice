c613b056dd3323f6d2ba9dc927f808d7d65a67f7
connorthemiller <checkthegate21@gmail.com>
2019-12-30 10:04:41 -0800

    partway through making database

--- server/src/app.js
+++ server/src/app.js
@@ -4,15 +4,24 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
+const {sequelize} = require('./models')
+const config = require('./config/config')
+
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.post('/register', (req, res) => {
-    res.send({
-        message: `hello ${req.body.email}! have fun.`
-    })
+require('./routes')(app)
+
+
+app.get('/', function(req, res){
+  res.send({message: "hello"})
 })
 
-app.listen(process.env.PORT || 8081)
+sequelize.sync()
+  .then(()=>{
+    app.listen(config.port)
+    console.log(`Server started on port ${config.port}`)
+  })
+
---
+++ server/src/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  port: 8081,
+  db: {
+    database: process.env.DB_NAME || 'mycoolapp'
+    user:
+    password:
+    options: {
+      
+    }
+  }
+}
