cc2d5dca938745ee08c6b40d5849a84ae7a952d0
connorthemiller <connormiller@Connors-Mac-mini.local>
2019-12-30 13:17:04 -0800

    not sure

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,7 +1,15 @@
+const {User} = require('../models')
+
 module.exports = {
-  register (req, res) {
-    res.send({
-        message: `hello ${req.body.email}! have fun.`
-    })
+  async register (req, res) {
+    console.log(req.body)
+    try {
+      const user = await User.create(req.body)
+      res.send(user.toJSON())
+    } catch (err) {
+      res.status(400).send({
+        error: 'This email account is already in use.'
+      })
+    }
   }
 }
