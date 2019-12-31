bacdbdb71f390dc79a8a32b3235e4b83be330e5f
connorthemiller <connor.c.miller@gmail.com>
2019-12-31 14:01:06 -0800

    working code, no vuetify

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
+    <h1>Register</h1>
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email" />
+    <br>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password" />
+      <br>
+      <button
+        @click="register">
+        Register
+      </button>
+      <div class="error" v-html="error"></div>
   </div>
 </template>
 
