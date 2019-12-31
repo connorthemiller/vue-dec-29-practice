03a3f73160f76a364e7e6977037f4dd884c1ec40
connorthemiller <connor.c.miller@gmail.com>
2019-12-31 14:47:41 -0800

    vuetify worksgit add --all

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,54 +1,51 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input
-      type="email"
-      name="email"
-      v-model="email"
-      placeholder="email" />
-    <br>
-    <input
-      type="password"
-      name="password"
-      v-model="password"
-      placeholder="password" />
-      <br>
-      <button
-        @click="register">
-        Register
-      </button>
-      <div class="error" v-html="error"></div>
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input type="email" name="email" v-model="email" placeholder="email" />
+          <br />
+          <input type="password" name="password" v-model="password" placeholder="password" />
+          <br />
+          <v-btn class="cyan" @click="register">Register</v-btn>
+          <div class="error" v-html="error"></div>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-import AuthenticationService from '@/services/AuthenticationService'
+import AuthenticationService from "@/services/AuthenticationService";
 export default {
-  data () {
+  data() {
     return {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
       error: null
-    }
+    };
   },
   methods: {
-    async register () {
+    async register() {
       try {
         await AuthenticationService.register({
           email: this.email,
           password: this.password
-        })
-        this.error = null
+        });
+        this.error = null;
       } catch (error) {
-        this.error = error.response.data.error
+        this.error = error.response.data.error;
       }
     }
   }
-}
+};
 </script>
 
 <style scoped>
-  .error {
-    color: red;
-  }
+.error {
+  color: red;
+}
 </style>
